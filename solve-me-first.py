710501f0b89a411c4990d1ad56209774f973dbb1
Saif Ali <saif.blt17@gmail.com>
2024-09-09 11:14:18 +0530

    sum of 2 nums

---
+++ solve-me-first.py
@@ -0,0 +1,11 @@
+#link - https://www.hackerrank.com/challenges/solve-me-first/problem
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a+b
+
+
+A = 4943438482
+B = 2832832838
+
+print(solveMeFirst(A, B))
